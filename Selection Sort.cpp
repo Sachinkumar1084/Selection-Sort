1cb75cfccd1f44cd7294ef34956fb00e48ed89ae
Sachin Kumar <122540894+Sachinkumar1084@users.noreply.github.com>
2024-07-05 21:36:02 +0530

    Add files via upload

---
+++ Selection Sort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void selectionsort(int* arr, int size){
+    for(int i = 0; i < size - 1; i++){
+        for(int j = i + 1; j < size; j++){
+            if(arr[j] < arr[i]){
+                swap(arr[j], arr[i]);
+            }  
+        }
+    }
+}
+
+void printSorted(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int arr[10] = {6, 8, 4, 9, 5, 90, 36, 75, 2, 64};
+    int size = 10;
+    cout << "Sorted array:" << endl;
+    selectionsort(arr, size);
+    printSorted(arr, size);
+    return 0;
+}
